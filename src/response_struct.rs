448bda4fc3c15160176260ff47dd0c6fc14105bd
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-28 15:24:51 +0900

    cargo fmt

--- src/response_struct.rs
+++ src/response_struct.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct Search {
-    pub result: Vec<String>
+    pub result: Vec<String>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -11,10 +11,10 @@ pub struct Info {
     pub start_time: String,
     pub end_time: String,
     pub next_holiday: Option<String>,
-    pub distance: Option<u64>
+    pub distance: Option<u64>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct Location {
-    pub result: Vec<Info>
+    pub result: Vec<Info>,
 }
--- src/router.rs
+++ src/router.rs
@@ -11,36 +11,40 @@ pub async fn search(req: Request<()>) -> tide::Result<Body> {
 
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
 
-    let row = sqlx::query!(r#"
+    let row = sqlx::query!(
+        r#"
         SELECT mart_name
         FROM   mart
         WHERE  mart_type = $1 AND mart_name LIKE $2;
         "#,
         mart,
-        format!("%{}%", keyword))
-        .fetch_all(pg_conn.acquire().await?)
-        .await?;
+        format!("%{}%", keyword)
+    )
+    .fetch_all(pg_conn.acquire().await?)
+    .await?;
 
     Body::from_json(&Search {
-        result: row.iter().map(|rec| rec.mart_name.clone()).collect()
+        result: row.iter().map(|rec| rec.mart_name.clone()).collect(),
     })
 }
 
 pub async fn info(req: Request<()>) -> tide::Result<Body> {
     let mart = req.param("mart")?;
     let name = decode(req.param("name")?)?.to_string();
-    
+
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
 
-    let row = sqlx::query!(r#"
+    let row = sqlx::query!(
+        r#"
         SELECT mart_name, start_time, end_time, next_holiday
         FROM   mart
         WHERE  mart_type = $1 AND mart_name LIKE $2;
         "#,
         mart,
-        name)
-        .fetch_one(pg_conn.acquire().await?)
-        .await?;
+        name
+    )
+    .fetch_one(pg_conn.acquire().await?)
+    .await?;
 
     Body::from_json(&Info {
         name: row.mart_name,
@@ -48,9 +52,9 @@ pub async fn info(req: Request<()>) -> tide::Result<Body> {
         end_time: row.end_time.to_string(),
         next_holiday: match row.next_holiday {
             Some(date) => Some(date.to_string()),
-            None => None
+            None => None,
         },
-        distance: None
+        distance: None,
     })
 }
 
@@ -60,7 +64,8 @@ pub async fn location(req: Request<()>) -> tide::Result<Body> {
 
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
 
-    let row = sqlx::query!(r#"
+    let row = sqlx::query!(
+        r#"
         SELECT * FROM (
             SELECT mart_name, start_time, end_time, next_holiday, 
                   ST_DistanceSphere(ST_GeomFromText($1), loc) AS distance
@@ -69,24 +74,27 @@ pub async fn location(req: Request<()>) -> tide::Result<Body> {
         ORDER BY distance
         LIMIT  10
         "#,
-        format!("POINT({} {})", lon, lat))
-        .fetch_all(pg_conn.acquire().await?)
-        .await?;
+        format!("POINT({} {})", lon, lat)
+    )
+    .fetch_all(pg_conn.acquire().await?)
+    .await?;
 
     Body::from_json(&Location {
-        result: row.iter().map(|rec| Info {
-            name: rec.mart_name.clone(),
-            start_time: rec.start_time.to_string(),
-            end_time: rec.end_time.to_string(),
-            next_holiday: match rec.next_holiday {
-                Some(date) => Some(date.to_string()),
-                None => None
-            },
-            distance: Some(match rec.distance {
-                Some(dist) => dist as u64,
-                None => 0
+        result: row
+            .iter()
+            .map(|rec| Info {
+                name: rec.mart_name.clone(),
+                start_time: rec.start_time.to_string(),
+                end_time: rec.end_time.to_string(),
+                next_holiday: match rec.next_holiday {
+                    Some(date) => Some(date.to_string()),
+                    None => None,
+                },
+                distance: Some(match rec.distance {
+                    Some(dist) => dist as u64,
+                    None => 0,
+                }),
             })
-        })
-        .collect()
+            .collect(),
     })
 }
