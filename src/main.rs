2c844bc12c54918ffab37df2f05ba5a19cfeb3ba
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-28 14:16:41 +0900

    Support database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
 
 use async_std::task;
+use dotenv::dotenv;
 use eyre::Result;
+use sqlx::Postgres;
+use tide_sqlx::SQLxMiddleware;
 
 mod response_struct;
 mod router;
 
 fn main() -> Result<()> {
+    dotenv().ok();
+    
     let host = env::var("HOST").unwrap_or("0.0.0.0".to_string());
     let port: u16 = env::var("PORT").unwrap_or("4000".to_string()).parse()?;
     task::block_on(start(&host, port))?;
@@ -15,9 +20,12 @@ fn main() -> Result<()> {
 
 async fn start(host: &str, port: u16) -> Result<()> {
     let mut app = tide::new();
+    app.with(SQLxMiddleware::<Postgres>::new(&env::var("DATABASE_URL").unwrap()).await?);
+
     app.at("/search/:mart/:keyword").get(router::search);
     app.at("/info/:mart/:name").get(router::info);
     app.at("/location/:lat/:lon").get(router::location);
+
     app.listen(format!("{}:{}", host, port)).await?;
     Ok(())
 }
--- src/router.rs
+++ src/router.rs
@@ -1,4 +1,6 @@
+use sqlx::{Acquire, Postgres};
 use tide::{Body, Request};
+use tide_sqlx::SQLxRequestExt;
 use urlencoding::decode;
 
 use crate::response_struct::{Info, Location, Search};
@@ -6,21 +8,46 @@ use crate::response_struct::{Info, Location, Search};
 pub async fn search(req: Request<()>) -> tide::Result<Body> {
     let mart = req.param("mart")?;
     let keyword = decode(req.param("keyword")?)?;
-    println!("{}, {}", mart, keyword);
+
+    let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+
+    let row = sqlx::query!(
+        r#"SELECT mart_name
+        FROM   mart
+        WHERE  mart_type = $1 AND mart_name LIKE $2;"#,
+        mart,
+        format!("%{}%", keyword))
+        .fetch_all(pg_conn.acquire().await?)
+        .await?;
+
     Body::from_json(&Search {
-        result: vec![]
+        result: row.iter().map(|rec| rec.mart_name.clone()).collect()
     })
 }
 
 pub async fn info(req: Request<()>) -> tide::Result<Body> {
     let mart = req.param("mart")?;
-    let name = decode(req.param("name")?)?;
-    println!("{}, {}", mart, name);
+    let name = decode(req.param("name")?)?.to_string();
+    
+    let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+
+    let row = sqlx::query!(
+        r#"SELECT mart_name, start_time, end_time, next_holiday
+        FROM   mart
+        WHERE  mart_type = $1 AND mart_name LIKE $2;"#,
+        mart,
+        name)
+        .fetch_one(pg_conn.acquire().await?)
+        .await?;
+
     Body::from_json(&Info {
-        name: format!("{} {}", mart, name),
-        start_time: "10:00:00".to_string(),
-        end_time: "22:00:00".to_string(),
-        next_holiday: "2021/10/27".to_string(),
+        name: row.mart_name,
+        start_time: row.start_time.to_string(),
+        end_time: row.end_time.to_string(),
+        next_holiday: match row.next_holiday {
+            Some(date) => Some(date.to_string()),
+            None => None
+        },
         distance: None
     })
 }
@@ -34,7 +61,7 @@ pub async fn location(req: Request<()>) -> tide::Result<Body> {
                 name: "이마트 가든5점".to_string(),
                 start_time: "10:00:00".to_string(),
                 end_time: "22:00:00".to_string(),
-                next_holiday: "2021/10/27".to_string(),
+                next_holiday: Some("2021/10/27".to_string()),
                 distance: Some(1254)
             }
         ]
