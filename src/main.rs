2ae03cefdbd9cd88539273b7401be2f9ec0d6e3a
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-28 14:17:56 +0900

    Move database_url to main

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,17 @@ mod router;
 
 fn main() -> Result<()> {
     dotenv().ok();
-    
+
     let host = env::var("HOST").unwrap_or("0.0.0.0".to_string());
     let port: u16 = env::var("PORT").unwrap_or("4000".to_string()).parse()?;
-    task::block_on(start(&host, port))?;
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not served");
+    task::block_on(start(&host, port, &database_url))?;
     Ok(())
 }
 
-async fn start(host: &str, port: u16) -> Result<()> {
+async fn start(host: &str, port: u16, database_url: &str) -> Result<()> {
     let mut app = tide::new();
-    app.with(SQLxMiddleware::<Postgres>::new(&env::var("DATABASE_URL").unwrap()).await?);
+    app.with(SQLxMiddleware::<Postgres>::new(database_url).await?);
 
     app.at("/search/:mart/:keyword").get(router::search);
     app.at("/info/:mart/:name").get(router::info);
