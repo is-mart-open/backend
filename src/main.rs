0789246fc5fb3da67ae888f84618b0d821afc498
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-10 18:51:28 +0900

    Added CORS url

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn start(host: &str, port: u16, database_url: &str) -> Result<()> {
 
     let mut app = tide::new();
     app.with(SQLxMiddleware::<Postgres>::new(database_url).await?);
-    app.with(CorsMiddleware::new().allow_origin(vec!["http://localhost:3000", "https://is-mart-open.btry.dev"]));
+    app.with(CorsMiddleware::new().allow_origin(vec!["http://localhost:3000", "http://10.0.1.4:3000", "https://is-mart-open.btry.dev"]));
 
     app.at("/search/:mart").get(router::search);
     app.at("/search/:mart/:keyword").get(router::search);
